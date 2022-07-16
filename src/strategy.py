19482cb6f816b4eb9779ed5f624c888bc1cd48a9
xiachunhao <xiachunhao@jd.com>
2022-07-16 23:03:50 +0800

    设计遗传算法各种算子结构

--- src/strategy.py
+++ src/strategy.py
@@ -86,17 +86,52 @@ class Strategy:
 
 
     @classmethod
-    def crossover_operator(cls):
-        '''交叉算子'''
-        # Machine Selection part (MS两种) - two point crossover、uniform crossover
-        if random.random() < 0.5:
-            # two point crossover
-            pass
-        else:
-            # uniform crossover
-            pass
+    def crossover_operator(cls,mate_pool,instance):
+        '''交叉算子(等位基因) - 文献中选择了三次，此处选择一次'''
+        if len(mate_pool) <= 2:
+            print("进化池中个体数量太少....")
+            exit(0)
+        parent_f_index,parent_m_index = list(np.random.random(low=0,high=len(mate_pool),size=2))
+        parent_f = mate_pool[parent_f_index]
+        parent_m = mate_pool[parent_m_index]
+        crossover_flag = True
+        while crossover_flag:
+            # Machine Selection part (MS两种) - two point crossover、uniform crossover
+            if random.random() < 0.5:
+                # two point crossover
+                first_position = np.random.random(low=0,high=len(parent_f[0])-1)
+                second_position = first_position + 1
+            else:
+                # uniform crossover
+                first_position,second_position = list(np.random.random(low=0,high=len(parent_f[0]),size=2))
+            # 检查对应工序是否有这么多候选机器可选，如果没有则重新选择
+            if not cls.new_machine_effective(instance,parent_f,parent_m,first_position,second_position):
+                continue
+            crossover_flag = False
+
+
         # Operation Sequence part(OS - POX)
         pass
+
+
+    @classmethod
+    def new_machine_effective(cls,instance,parent_f,parent_m,first_position,second_position):
+        '''必须保证交换后，机器对应的是工序候选数量内'''
+        if cls.check_alleles_effective(instance,parent_f,parent_m,first_position) and cls.check_alleles_effective(instance,parent_f,parent_m,second_position):
+            return True
+        return False
+
+
+    @classmethod
+    def check_alleles_effective(cls,instance,parent_f,parent_m,check_position):
+        '''检查等位基因是否可以互换'''
+        parent_f_product, parent_f_no = parent_f[1][check_position].split('-')
+        parent_m_product, parent_m_no = parent_m[1][check_position].split('-')
+        parent_f_machine = instance.equ_dict[instance.process_flow_dict[instance.product_dict[parent_f_product].route_id][parent_f_no - 1].equ_type]
+        parent_m_machine = instance.equ_dict[instance.process_flow_dict[instance.product_dict[parent_m_product].route_id][parent_m_no - 1].equ_type]
+        if len(parent_f_machine) >= parent_m[0][check_position] and len(parent_m_machine) >= parent_f[0][check_position]:
+            return True
+        return False
 
 
     @classmethod
